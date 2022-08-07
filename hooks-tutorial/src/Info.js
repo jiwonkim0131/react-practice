81a51e7cf4d48e585d0a6d6945880af60b8d3ed6
jiownkim0131 <qhsh866@gmail.com>
2022-08-07 16:44:53 +0900

    feat: useEffect 실습 - 마운트 될 때마 실행

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -6,8 +6,11 @@ const Info = () => {
   const [nickName, setNickName] = useState('');
   useEffect(() => {
     // 1. useEffect 기본 : 컴포넌트가 렌더링 될 때마다 콘솔이 출력된다
-    console.log('렌더링이 완료되었습니다.');
-  });
+    // console.log('렌더링이 완료되었습니다.');
+
+    // 2. useEffect 마운트 될 때마 특정 함수 실행 : 두 번째 인자로 빈 배열 전달
+    console.log('마운트 될 때만 실행');
+  }, []);
 
   const onChangeName = ({ target }) => setName(target.value);
   const onChangeNickName = ({ target }) => setNickName(target.value);
